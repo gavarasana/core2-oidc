3fef4d45c4599d112a820e26c09510e46568a832
Ravi Gavarasana <gavarasana@gmail.com>
2018-10-03 20:44:40 -0400

    Adding userinfo end point

--- ravi.learn.idp.web/Controllers/GalleryController.cs
+++ ravi.learn.idp.web/Controllers/GalleryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
@@ -192,6 +193,23 @@ namespace ravi.learn.idp.web.Controllers
             // will clear out the cookie
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        public async Task<IActionResult> OrderFrame()
+        {
+            var accessToken = await HttpContext.GetTokenAsync(OpenIdConnectDefaults.AuthenticationScheme, OpenIdConnectParameterNames.AccessToken);
+
+            var discoveryClient = new DiscoveryClient("https://localhost:44313/");
+            var metadataResponse = await discoveryClient.GetAsync();
+            var userinfoClient = new UserInfoClient(metadataResponse.UserInfoEndpoint);
+            var response = await userinfoClient.GetAsync(accessToken);
+            if (response.IsError)
+            {
+                throw new Exception("Problem accessing the userInfo endpoint");
+            }
+
+            var orderFrameViewModel = new OrderFrameViewModel("address");
+            return View(orderFrameViewModel);
         }
     }
 
